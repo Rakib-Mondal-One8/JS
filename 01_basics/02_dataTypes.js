3dbb5710f2ee9224ffa32c09836601f6cf437c44
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-03 03:29:03 +0530

    JS Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js as newer version
+// alert(3+3) This works on browser, We are running on Nodejs.
+
+console.log(3 + 3);
+console.log("Rakib");
+
+let name = "Rakib";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => Value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
