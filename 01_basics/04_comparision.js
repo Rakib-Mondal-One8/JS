76e75d1ddc67b74c413ca81c97e618b345cb096b
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-03 05:07:46 +0530

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2 > 5);
+// console.log(5 < 7);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+
+
+console.log('2' === 2); // === is strict conversion
