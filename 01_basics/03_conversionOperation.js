3d3ef42f2b29499e0b622fef709f3fb11e5836ff
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-03 04:55:56 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " rakib";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+
+// console.log("1" + 2 + 2);
