2a3e98f856862963fd3d5b104811f494e5ea22d0
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-03 04:11:05 +0530

    Type Casting

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = 33;
+
+// console.log(typeof score);
+
+// score = "33aaaa";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33aaaa" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
