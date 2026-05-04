bc219cc149fc1cd894afc9b16526679c1596af70
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-04 08:16:41 +0530

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,29 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//   if (index == 5) {
+//     console.log("5 is best number");
+//   }
+//   console.log(index);
+// }
+
+
+for(let i=0;i<=10;i++){
+    console.log(i);
+
+    for(let j=1;j<=5;j++){
+        console.log(j);
+        
+    }
+}
+
+
+
+
+let i = 0;
+
+while(i<5){
+    console.log(i);
+    i++;
+    
+}
