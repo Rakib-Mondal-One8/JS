eaf388b84fd9e203e228de053d89aaf0d4ab27c4
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-06 05:33:27 +0530

    Promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,79 @@
+const promiseOne = new Promise(function (resolve, reject) {
+  // Do an async task
+  // DB calls, cryptography, network
+  setTimeout(function () {
+    console.log("Async task is done");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Rakib", email: "rakib@google.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "arman", password: "12" });
+    } else {
+      reject("ERROR: Somthing wents wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Done");
+  });
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "arman", password: "12" });
+    } else {
+      reject("ERROR: Somthing wents wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
